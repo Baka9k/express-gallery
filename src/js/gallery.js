afe9707d7f6ef980f847f0d2c85d5c8d07120939
Baka <neoplanspaceliner9@gmail.com>
2016-09-28 02:35:18 +0300

    Update thumbnails layout

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,24 @@
+$( document ).ready(function() {
+	
+	//Add listener on #addImageModalTrigger button which opens form in modal for adding new image
+ 	$("#addImageModalTrigger").on('click', function (e) {
+		openModal("addImageModal");
+	});
+	
+	//Add "portrait" class to vertical images thubnails
+	$(".thumb").each(function(index) {
+		var img = $(this).find("img");
+		if ( $(img).width() < $(img).height() ) {
+			$(img).addClass("portrait");
+		} 
+	});
+	
+});
+
+
+
 function openModal(id) {
     $("#"+id).modal();
 }
 
-$("#addImageModalTrigger").on('click', function (e) {
-    openModal("addImageModal");
-});
 
