5b4e19414fbc4e8f974810065aaf676208c5437e
Baka <neoplanspaceliner9@gmail.com>
2016-10-18 19:43:44 +0300

    Add arrows in image viewer

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,14 +1,22 @@
-$(window).load(function() {
-	
-	//Add "portrait" class to vertical images thubnails
-	$(".thumb").each(function(index) {
-		var img = $(this).find("img");
-		if ( $(img).width() < $(img).height() ) {
-			$(img).addClass("portrait");
-		} 
-	});
-	
-});
+var viewer = {
+	currentImage: "",
+	currentImageJQ: "",
+	prevImage: function() {
+		var next = $(this.currentImageJQ).parent().parent().prev().find("img");
+		if (next.length < 1) return;
+		this.currentImageJQ = next;
+		this.currentImage = $(this.currentImageJQ).attr("src").replace(/thumbnails\//, "/uploads/");
+	},
+	nextImage: function() {
+		var next = $(this.currentImageJQ).parent().parent().next().find("img");
+		if (next.length < 1) return;
+		this.currentImageJQ = next;
+		this.currentImage = $(this.currentImageJQ).attr("src").replace(/thumbnails\//, "/uploads/");
+	},
+	update: function() {
+		renderImageInModal(this.currentImage);
+	},
+};
 
 
 $(document).ready(function() {
@@ -20,15 +28,18 @@ $(document).ready(function() {
 	
 	//Add listeners on thumbnails for opening images in modal
 	$(".square-thumbnail").each(function (index, value){
-		var link = $(this).find("img").attr("src");
+		var thumbLink = $(this).find("img").attr("src");
+		var link = thumbLink.replace(/thumbnails\//, "/uploads/");
 		$(this).on('click', function (e) {
 			openModal("imageViewerModal");
-			renderImageInModal(link.replace(/thumbnails\//, ""));
+			renderImageInModal(link);
+			viewer.currentImage = link;
+			viewer.currentImageJQ = $(this).find("img");
 		});
 	});
 	
 	$("#openInNewTabButton").on('click', function (e) {
-		var imageLink = $("#imageViewer").attr("src");
+		var imageLink = viewer.currentImage;
 		var win = window.open(imageLink, '_blank');
 		if (win) {
 			win.focus();
@@ -36,7 +47,31 @@ $(document).ready(function() {
 			alert('Please allow popups for this website');
 		}
 	});
+	
+	$("#leftArrow").on('click', function (e) {
+		viewer.prevImage();
+		viewer.update();
+	});
+	$("#rightArrow").on('click', function (e) {
+		viewer.nextImage();
+		viewer.update();
+	});
 		
+});
+
+
+
+
+$(window).load(function() {
+	
+	//Add "portrait" class to vertical images thumbnails
+	$(".thumb").each(function(index) {
+		var img = $(this).find("img");
+		if ( $(img).width() < $(img).height() ) {
+			$(img).addClass("portrait");
+		} 
+	});
+	
 });
 
 
@@ -47,9 +82,8 @@ function openModal(id) {
 }
 
 function renderImageInModal(imagePath) {
-	var fullImagePath = "uploads/" + imagePath;
 	$("#imageViewerModal")
-		.find("img").attr("src", fullImagePath)
+		.find("img").attr("src", imagePath)
 		.css("max-height", $(window).height() - 120)
 		.parent().css("min-height", $(window).height() - 120);
 }
